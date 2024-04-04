a5ec831ea4d196bbb614eb986e07e1d322ad781e
Beatrizsr96 <1231824@isep.ipp.pt>
2024-04-04 10:49:41 +0100

    Close #17 - extracted zip folder content into CA2/Part2 folder

---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/test/java/com/greglturnquist/playroll/ReactAndSpringDataRestBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.playroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringDataRestBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
