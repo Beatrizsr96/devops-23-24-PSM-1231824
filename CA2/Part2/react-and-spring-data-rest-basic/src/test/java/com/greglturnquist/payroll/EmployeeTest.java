410d4d4b1574df030b89f402c3e5983a495da758
Beatrizsr96 <1231824@isep.ipp.pt>
2024-04-04 11:09:18 +0100

    Close #18 - Copied src folder, package.js and webpack.config.js and deleted unnecessary folders from CA2/Part2

---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -0,0 +1,130 @@
+package com.greglturnquist.payroll;
+
+import org.apache.naming.factory.SendMailFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+	@Test
+	void validEmployeeCreated() {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		Employee employee = new Employee(firstName,lastName,description,jobYears,email);
+		assertNotNull(employee);
+	}
+
+	@Test
+	void invalidEmployeeCreatedFirstNameEmpty() throws IllegalArgumentException {
+		String firstName = "";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedFirstNameNull() throws IllegalArgumentException {
+		String firstName = null;
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedLastNameEmpty() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "";
+		String description = "ring bearer";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedLastNameNull() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = null;
+		String description = "ring bearer";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedDescriptionEmpty() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "";
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedCreatedNull() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = null;
+		int jobYears = 50;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void validEmployeeCreatedZeroJobYears() {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 0;
+		String email = "frodo.baggins@live.org";
+		Employee employee = new Employee(firstName,lastName,description,jobYears,email);
+		assertNotNull(employee);		;
+	}
+
+	@Test
+	void invalidEmployeeCreatedJobYearsNegativeValue() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = -1;
+		String email = "frodo.baggins@live.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedEmptyEmail() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 1;
+		String email = null;
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedNullEmail() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 1;
+		String email = "";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+
+	@Test
+	void invalidEmployeeCreatedEmailWithoutArroba() throws IllegalArgumentException {
+		String firstName = "Frodo";
+		String lastName = "Baggins";
+		String description = "ring bearer";
+		int jobYears = 1;
+		String email = "frodo.bagginslive.org";
+		assertThrows(IllegalArgumentException.class, () -> new Employee(firstName,lastName,description,jobYears,email));
+	}
+}
